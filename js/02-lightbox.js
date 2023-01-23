36fa85ad7fbebeae99242a676e58b9762f114bd5
Nikita_Hnitetskyi <108988090+SmyslovaVictoria@users.noreply.github.com>
2023-01-23 18:47:06 +0200

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function createGalleryCard(cardItems) {
+  return cardItems
+    .map(({ preview, original, description }) => {
+      return `
+            <a class= "gallery__item" href="${original}" data-lightbox = "lbox">
+                <img
+                    class= "gallery__image"
+                    src="${preview}"
+                    alt="${description}"
+                    />
+             </a>`;
+    })
+    .join("");
+};
+
+const galleryList = createGalleryCard(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+
+const lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
